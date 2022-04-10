1ca71a5dfee0a7166f9c4cf20aa03d0c61acff2b
Yali Zak-Malkin <yalizakmalkin@gmail.com>
2022-04-10 06:48:44 +0300

    added queue ADT

--- Adv_C_Exe3/Queue.c
+++ Adv_C_Exe3/Queue.c
@@ -1,30 +1,56 @@
 #include "Queue.h"
+#include <stdio.h>
 
 /***************** Queue ADT Implementation *****************/
 
 void initQueue(Queue* q)
 {
-	// add your code here
+	q->head = NULL;
+	q->tail = NULL;
 }
 
 void destroyQueue(Queue* q)
 {
-	// add your code here
+	intNode* tmp;
+	while (q->head != NULL) {
+		tmp = q->head;
+		q->head = q->head->next;
+		free(tmp);
+	}
 }
 
 void enqueue(Queue* q, unsigned int data)
 {
-	// add your code here
+	intNode* p = (intNode*)malloc(sizeof(intNode));
+	//check allocation code added here…
+	p->data = data; // set element data
+	p->next = NULL;
+	if (!isEmpty(q)) q->tail->next = p; // not Empty?
+	//Add after last:
+	else q->head = p; // Otherwise – it becomes
+	//first
+	q->tail = p; // update queue tail
 }
 
 unsigned int dequeue(Queue* q)
 {
-	// add your code here
+	if (isEmpty(q))
+	{
+		printf("Cannot dequeue an empty queue!");
+		return 0;
+	}
+	int temp = q->head->data;
+	intNode* p = q->head;
+	q->head = q->head->next;
+	if (q->head == NULL) //if queue gets empty
+		q->tail = NULL;//both front and rear NULL
+	free(p);
+	return temp;
 }
 
 int isEmptyQueue(const Queue* q)
 {
-	// add your code here
+	return q->head == NULL && q->tail == NULL;
 }
 
 /***************** Functions using Queues - Implementation/definition **************************/
